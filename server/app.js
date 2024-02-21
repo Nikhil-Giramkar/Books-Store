97d91cab4351cd41fe53ad38ad209d4c98ea331c
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-21 18:13:13 +0530

    GraphQL installed

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
+const {graphqlHTTP} = require("express-graphql")
+
 const app = express();
+
+//We will use graphql by adding it as a middleware at a route
+//The below code will trigger graphqlHTTP() with options when someone hits /graphql route
+app.use("/graphql", graphqlHTTP({
+
+}));
 
 app.listen(4000, ()=>{
     console.log("Listening at http://localhost:4000");
