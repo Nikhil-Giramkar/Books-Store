c40c03f363ad79dda275b583449ffb4172fb6bbf
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-23 16:46:40 +0530

    useState for Book and handle onchange and onSubmit

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -1,23 +1,45 @@
+import { useState } from "react"
 import { AuthorsList } from "./AuthorsList"
 
 export const AddBook = () => {
 
+    const [book, setBook] = useState({
+        name: "",
+        genre: "",
+        authorId: ""
+    })
+
+    const handleOnChange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        
+        setBook({
+            ...book,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(book);
+    }
+
     return (
         <>
-            <form id="add-book">
+            <form id="add-book" onSubmit={handleSubmit}>
                 <div className="field">
-                    <label htmlFor="bookName">Book Name: </label>
-                    <input type="text" name="bookName" id="bookName" />
+                    <label htmlFor="name">Book Name: </label>
+                    <input type="text" name="name" id="name" onChange={handleOnChange} value={book.name} />
                 </div>
 
                 <div className="field">
                     <label htmlFor="genre">Genre: </label>
-                    <input type="text" name="genre" id="genre" />
+                    <input type="text" name="genre" id="genre" onChange={handleOnChange} value={book.genre}/>
                 </div>
 
                 <div className="field">
                     <label htmlFor="author">Author: </label>
-                    <select>
+                    <select name="authorId" onChange={handleOnChange}>
                         <option>Select Author</option>
                         <AuthorsList />
                     </select>
