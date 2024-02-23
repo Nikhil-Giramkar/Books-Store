e678eaf7f6feed43b7538b1799f8478550756a52
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-23 15:28:42 +0530

    Cors error handled

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const app = express();
+
+const cors = require('cors');
+//Handling CORS policy issue
+//Need to mention cors option else data won't be saved in DB
+const corsOptions = {
+    origin: "http://localhost:5173", //frontend URL
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD", //Types of requests to accept from above URL
+    credentials: true,
+}
+app.use(cors(corsOptions));
 
 const {graphqlHTTP} = require("express-graphql")
 
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'});
+
 
 const connectDB = require("./db/conn");
 
@@ -12,8 +24,6 @@ const PORT = process.env.PORT
 
 //import schema
 const schema = require("./schema/schema")
-
-const app = express();
 
 //We will use graphql by adding it as a middleware at a route
 //The below code will trigger graphqlHTTP() with options when someone hits /graphql route
