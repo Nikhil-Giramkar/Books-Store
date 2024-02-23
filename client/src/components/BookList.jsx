052a4eb260d468fcc32fea8810063db36db64e54
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-23 15:21:52 +0530

    Created a getBooksQuery via gql - ApolloClient

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,11 +1,31 @@
+import { gql, useQuery } from "@apollo/client"
+
+const getBooksQuery = gql`
+{
+    books{
+        name
+        genre
+        id
+    }
+}
+`
+
 export const BookList = () => {
-    return(
+
+    const {loading, error, data} = useQuery(getBooksQuery);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    //Lets see what we get
+    console.log(data)
+    return (
         <>
-        <div>
-            <ul id="book-list">
-                <li>Book Name</li>
-            </ul>
-        </div>
+            <div>
+                <ul id="book-list">
+                    <li>Book Name</li>
+                </ul>
+            </div>
         </>
     )
 }
